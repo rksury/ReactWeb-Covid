53fc44a5884a7244ae2fd30dedf540faba2af7d4
Rksury <rksury@gmail.com>
2020-09-22 03:13:00 +0530

    create production

---
+++ build/precache-manifest.28806c41d66923eb01cefb43aa37547d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a651ab08e431ba5456ceda13eba4dd8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a54a58a8bdabd474c63d",
+    "url": "/static/css/2.a4deadbc.chunk.css"
+  },
+  {
+    "revision": "d22b96dde2642d13accc",
+    "url": "/static/css/main.04ab0ce3.chunk.css"
+  },
+  {
+    "revision": "a54a58a8bdabd474c63d",
+    "url": "/static/js/2.ea23a715.chunk.js"
+  },
+  {
+    "revision": "1a00154298fce8f79ea8556d30a19b60",
+    "url": "/static/js/2.ea23a715.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d22b96dde2642d13accc",
+    "url": "/static/js/main.da21a683.chunk.js"
+  },
+  {
+    "revision": "86ff7c4bb1e41652c5b1",
+    "url": "/static/js/runtime-main.eeca2f94.js"
+  },
+  {
+    "revision": "62c8c67efd8995a523831e17a36ea7f2",
+    "url": "/static/media/logo.62c8c67e.png"
+  },
+  {
+    "revision": "1f48aa764c101f42fc1bc6514b9fcf8d",
+    "url": "/static/media/profile.1f48aa76.png"
+  }
+]);

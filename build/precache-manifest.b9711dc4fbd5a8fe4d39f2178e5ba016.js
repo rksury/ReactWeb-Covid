24c0d1d09f91a8ca8a8ed69b5d59f1b0d782add5
Rksury <rksury@gmail.com>
2020-09-23 03:13:49 +0530

    update json

---
+++ build/precache-manifest.b9711dc4fbd5a8fe4d39f2178e5ba016.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d92c8307f33fc4e7b486c23d13292d5c",
+    "url": "/CovidTest/index.html"
+  },
+  {
+    "revision": "85630983dd669176cf34",
+    "url": "/CovidTest/static/css/2.a4deadbc.chunk.css"
+  },
+  {
+    "revision": "77ad8d0ca4e634b59aef",
+    "url": "/CovidTest/static/css/main.04ab0ce3.chunk.css"
+  },
+  {
+    "revision": "85630983dd669176cf34",
+    "url": "/CovidTest/static/js/2.8f28b4b4.chunk.js"
+  },
+  {
+    "revision": "1a00154298fce8f79ea8556d30a19b60",
+    "url": "/CovidTest/static/js/2.8f28b4b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77ad8d0ca4e634b59aef",
+    "url": "/CovidTest/static/js/main.63481d70.chunk.js"
+  },
+  {
+    "revision": "28e0bae95d730148a3cf",
+    "url": "/CovidTest/static/js/runtime-main.e0e145f5.js"
+  },
+  {
+    "revision": "62c8c67efd8995a523831e17a36ea7f2",
+    "url": "/CovidTest/static/media/logo.62c8c67e.png"
+  },
+  {
+    "revision": "1f48aa764c101f42fc1bc6514b9fcf8d",
+    "url": "/CovidTest/static/media/profile.1f48aa76.png"
+  }
+]);
